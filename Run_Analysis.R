6d3bb8402fa7e77d34bdf57aa5868b90da843939
Nikhil Gupta <gupta.nikhil29@yahoo.in>
2021-05-25 14:26:56 +0530

    Final PUSH

---
+++ Run_Analysis.R
@@ -0,0 +1,68 @@
+library(reshape2)
+
+
+#1. get dataset from web
+RawDatDir <- "./rawData"
+RawDatURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+RawDatFile <- "rawData.zip"
+rawDataDFn <- paste(RawDatDir, "/", "rawData.zip", sep = "")
+dataDir <- "./data"
+
+if (!file.exists(RawDatDir)) {
+  dir.create(RawDatDir)
+  download.file(url = RawDatURL, destfile = rawDataDFn)
+}
+if (!file.exists(dataDir)) {
+  dir.create(dataDir)
+  unzip(zipfile = rawDataDFn, exdir = dataDir)
+}
+
+
+#2.Merging the datasets
+
+# train data
+x_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/X_train.txt"))
+y_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/Y_train.txt"))
+s_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/subject_train.txt"))
+
+# test data
+x_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/X_test.txt"))
+y_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/Y_test.txt"))
+s_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/subject_test.txt"))
+
+# merged data
+x_data <- rbind(x_train, x_test)
+y_data <- rbind(y_train, y_test)
+s_data <- rbind(s_train, s_test)
+
+
+#3. load feature & activity info
+# feature info
+feature <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/features.txt"))
+
+# activity labels
+a_label <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/activity_labels.txt"))
+a_label[,2] <- as.character(a_label[,2])
+
+# extract feature cols & names named 'mean, std'
+selectedCols <- grep("-(mean|std).*", as.character(feature[,2]))
+selectedColNames <- feature[selectedCols, 2]
+selectedColNames <- gsub("-mean", "Mean", selectedColNames)
+selectedColNames <- gsub("-std", "Std", selectedColNames)
+selectedColNames <- gsub("[-()]", "", selectedColNames)
+
+
+#4. extract data by cols & using descriptive name
+x_data <- x_data[selectedCols]
+allData <- cbind(s_data, y_data, x_data)
+colnames(allData) <- c("Subject", "Activity", selectedColNames)
+
+allData$Activity <- factor(allData$Activity, levels = a_label[,1], labels = a_label[,2])
+allData$Subject <- as.factor(allData$Subject)
+
+
+#5. final Tidy datatset
+TempDat <- melt(allData, id = c("Subject", "Activity"))
+tidyData <- dcast(TempDat, Subject + Activity ~ variable, mean)
+
+write.table(tidyData, "./Final_tidy_dataset.txt", row.names = FALSE, quote = FALSE)
